f5e4636531264740c29ff45db854c84ff1b4ed3a
BatteryShark <batteryshark@users.noreply.github.com>
2021-08-24 21:54:05 -0400

    minor updates

--- VXAppUtils.ps1
+++ VXAppUtils.ps1
@@ -1,17 +1,17 @@
 $plugin_path = Join-Path -Path $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath('.\') -ChildPath Extensions\VXApp
 $repo_updater_path = Join-Path -Path $plugin_path -ChildPath RepoUpdater.ps1
 $exporter_path = Join-Path -Path $plugin_path -ChildPath VXAppExporter.ps1
-$emulator_path = Join-Path -Path $plugin_path -ChildPath "emulator\vxfe"
+$emulator_path = Join-Path -Path $plugin_path -ChildPath "emulator"
 Import-Module $repo_updater_path
 Import-Module $exporter_path
 
 
 
 function global:InstallUpdates{
-    if((DownloadLatestRelease "$plugin_path" "batteryshark" "VXAppUtils" "vxau.zip") -eq $false){
-        $PlayniteApi.Dialogs.ShowMessage("VXAppUtils Plugin is Up-to-Date.")
+    if((DownloadLatestRelease "$plugin_path" "batteryshark" "VXApp4Playnite" "VXApp.zip") -eq $false){
+        $PlayniteApi.Dialogs.ShowMessage("VXApp4Playnite Extension is Up-to-Date.")
     }else{
-        $PlayniteApi.Dialogs.ShowMessage("VXAppUtils Plugin has been Updated!")
+        $PlayniteApi.Dialogs.ShowMessage("VXApp4Playnite Extension has been Updated!")
     }
     if((DownloadLatestRelease "$emulator_path" "batteryshark" "VXAppFE" "vxfe.zip") -eq $false){
         $PlayniteApi.Dialogs.ShowMessage("VXApp Emulator is Up-to-Date.")
